d95617dd156ee8b0789fe4ac11fa4f25cc58ee55
fyb <1483824245@qq.com>
2020-05-05 17:30:42 +0800

    天猫的第1次提交

---
+++ src/com/baidu/tianmao/tm.java
@@ -0,0 +1,13 @@
+package com.baidu.tianmao;
+
+/**
+ * @author ：fyb
+ * @date ：Created in 2020/5/5
+ * @description ：
+ * @version: 1.0
+ */
+public class tm {
+    public static void main(String[] args) {
+        System.out.println("天猫的第一次提交");
+    }
+}
