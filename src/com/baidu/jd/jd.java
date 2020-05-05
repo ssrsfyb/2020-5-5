98f14674fd957388f77abe8fff69dc6224c0265c
fyb <1483824245@qq.com>
2020-05-05 17:25:51 +0800

    京东的第一次提交

---
+++ src/com/baidu/jd/jd.java
@@ -0,0 +1,13 @@
+package com.baidu.jd;
+
+/**
+ * @author ：fyb
+ * @date ：Created in 2020/5/5
+ * @description ：
+ * @version: 1.0
+ */
+public class jd {
+    public static void main(String[] args) {
+        System.out.println("京东的第一次提交");
+    }
+}
